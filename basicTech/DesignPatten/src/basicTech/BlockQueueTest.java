6f2e28def23b58d7a1b186239c675a53f60fe221
htuluo <search1985@163.com>
2017-06-29 18:21:34 +0800

    synchronize 使用，blockqueue

---
+++ basicTech/DesignPatten/src/basicTech/BlockQueueTest.java
@@ -0,0 +1,76 @@
+package basicTech;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by dell-3020 on 2017/6/29.
+ */
+public class BlockQueueTest {
+    private List<String> queue=new LinkedList<>();
+    private  int limit=10;
+
+    public synchronized  void    enQueue(String s) throws InterruptedException {
+        while (this.queue.size()==this.limit){
+            wait();
+        }
+        if (this.queue.size()==0){
+            notifyAll();
+        }
+        queue.add(s);
+
+    }
+    public synchronized  String    deQueue() throws InterruptedException {
+        while (this.queue.size()==0){
+            wait();
+        }
+        if (this.queue.size()==this.limit){
+            notifyAll();
+        }
+        String s1 = queue.get(0);
+        queue.remove(0);
+        return s1;
+
+    }
+
+    public static void main(String[] args) {
+        BlockQueueTest test=new BlockQueueTest();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (Integer i = 0; i < 10; i++) {
+
+                        test.enQueue(i + "22");
+                        Thread.sleep(60);
+                        if (i==6){
+                            Thread.sleep(100);
+                        }
+                        System.out.println("put i=" + i.toString());
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                    for (Integer i = 0; i < 20; i++) {
+
+                        String s = test.deQueue();
+                        System.out.println("take i=" + i.toString() + ";take-" + s);
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
--- basicTech/DesignPatten/src/basicTech/multiThread/hashMap.java
+++ basicTech/DesignPatten/src/basicTech/multiThread/hashMap.java
@@ -2,6 +2,27 @@ package basicTech.multiThread;
 
 public  class hashMap{
 
+    class  Node{
+    private  String key;
+    private  Object value;
+    private  Node next;
+
+
+    }
+    private  int capacity=10;
+    private  Node[] nodes=new  Node[capacity];
+
+
+ /*   public  put(String key,Object value){
+        Node node=new Node();
+        node.key=key;
+        node.value=value;
+        nodes.
+
+    }*/
+
+
+
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis()/1000);
     }
