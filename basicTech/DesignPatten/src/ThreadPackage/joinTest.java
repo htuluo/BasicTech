c0704be954586b4105499aa08532cff0f5d2ed87
htuluo <search1985@163.com>
2017-09-28 14:03:44 +0800

    数学运算

---
+++ basicTech/DesignPatten/src/ThreadPackage/joinTest.java
@@ -0,0 +1,42 @@
+package ThreadPackage;
+
+/**
+ * Created by dell-3020 on 2017/9/27.
+ */
+public class joinTest {
+    static volatile Integer count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread threada = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("1");
+            }
+        });
+
+        Thread threadb = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("2");
+            }
+        });
+        threada.start();
+        threadb.start();
+        while (true) {
+            if (count % 2 == 0) {
+                if (threada.isAlive()) {
+                    threada.wait();
+                }
+                threadb.notify();
+            } else {
+                threada.notify();
+                if (threadb.isAlive()) {
+
+                    threadb.wait();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ basicTech/DesignPatten/src/basicTech/CheckSum.java
@@ -0,0 +1,25 @@
+package basicTech;
+
+import java.text.MessageFormat;
+
+/**
+ * Created by dell-3020 on 2017/9/28.
+ */
+public class CheckSum {
+
+
+    public static void main(String[] args) {
+        int a,b,c,d;
+            c=1;
+
+        for (d=0;d<10;d++){
+            b=2*d+8;
+            a=d+9;
+            if((100*a+10*b+a)+(100*c+10*d+a)==(1000*1+100*d+10*a+b)){
+                System.out.println(MessageFormat.format("a={0},b={1},c={2},d={3}",a,b,c,d));
+                break;
+            }
+        }
+
+    }
+}
