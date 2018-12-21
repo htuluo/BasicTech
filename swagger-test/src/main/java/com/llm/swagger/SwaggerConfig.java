cc68eb13d712e7bd1cadfe639767c719367881fa
luoleiming <luoleiming@yingu.com>
2018-12-21 11:48:06 +0800

    可运行，可查看状态

--- swagger-test/src/main/java/com/llm/swagger/SwaggerConfig.java
+++ swagger-test/src/main/java/com/llm/swagger/SwaggerConfig.java
@@ -22,13 +22,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/MET-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/MET-INF/resources/webjars/");
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry){
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/MET-INF/resources/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/MET-INF/resources/webjars/");
+//    }
 
     @Bean
     public Docket createRestApi(){
