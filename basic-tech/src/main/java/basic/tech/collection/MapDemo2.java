10f86894c94c80312fc5ca8848cd45aab982ad54
luoleiming <luoleiming@yingu.com>
2019-06-28 16:47:29 +0800

    hashMap reference类的key

--- basic-tech/src/main/java/basic/tech/collection/MapDemo2.java
+++ basic-tech/src/main/java/basic/tech/collection/MapDemo2.java
@@ -11,14 +11,14 @@ import java.util.Random;
  * @history: (版本) 作者 时间 注释
  */
 public class MapDemo2 {
-    public static void main(String[] args){
-        HashMap h2=new HashMap();
-        for (int i=0;i<10;i++) {
+    public static void main(String[] args) {
+        HashMap h2 = new HashMap();
+        for (int i = 0; i < 10; i++) {
             h2.put(new Element(i), new Figureout());
         }
         System.out.println("h2:");
         System.out.println("Get the result for Element:");
-        Element test=new Element(5);
+        Element test = new Element(3);
         if (h2.containsKey(test)) {
             System.out.println((Figureout) h2.get(test));
         } else {
@@ -26,17 +26,31 @@ public class MapDemo2 {
         }
     }
 }
-class Element{
+
+class Element {
     int number;
-    public Element(int n){
-        number=n;
+
+    public Element(int n) {
+        number = n;
+    }
+
+    @Override
+    public int hashCode() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (o instanceof Element) && (number == ((Element) o).number);
     }
 }
-class Figureout{
-    Random r=new Random();
-    boolean possible=r.nextDouble()>0.5;
+
+class Figureout {
+    Random r = new Random();
+    boolean possible = r.nextDouble() > 0.5;
+
     @Override
-    public String toString(){
+    public String toString() {
         if (possible) {
             return "OK!";
         } else {
