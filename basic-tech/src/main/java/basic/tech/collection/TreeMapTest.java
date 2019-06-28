d525e2e7b3a6dff574f369143208b1b9fb50678b
luoleiming <luoleiming@yingu.com>
2019-06-28 17:03:49 +0800

    treeMap 顺序

--- basic-tech/src/main/java/basic/tech/collection/TreeMapTest.java
+++ basic-tech/src/main/java/basic/tech/collection/TreeMapTest.java
@@ -14,7 +14,11 @@ public class TreeMapTest {
     public static void main(String[] args) {
         TreeMap map=new TreeMap();
         map.put("aa","bb");
-        map.put("b","cc");
+        map.put("c","aa");
+        map.put("b","tt");
+        map.put("m","tt");
+        map.put("q","bt");
+        map.put("o","bs");
         System.out.println(map.toString());
     }
 }
