e9f962fcf8c1a66c4a312eb6edd4fa4877371d84
luoleiming <luoleiming@yingu.com>
2019-07-01 15:14:43 +0800

    整数的原子性试验，volatile 无效

--- basic-tech/src/main/java/basic/tech/atomic/AtomicIntegerDemo.java
+++ basic-tech/src/main/java/basic/tech/atomic/AtomicIntegerDemo.java
@@ -3,6 +3,7 @@ package basic.tech.atomic;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -13,10 +14,51 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @history: (版本) 作者 时间 注释
  */
 public class AtomicIntegerDemo {
-    public static AtomicInteger count =new AtomicInteger( 0);
+    public static AtomicInteger count = new AtomicInteger(0);
     public static int num = 20;
+    public static ExecutorService executor = Executors.newFixedThreadPool(20);
 
     public static void main(String[] args) {
+        threadPoolExecute();
+        threadArrayExecute();
+    }
+
+
+    private static void  threadPoolExecute2(){
+//        new Threadfactorybuilder
+//        ThreadFactory
+//        new ThreadPoolExecutor(5,20,5f, TimeUnit.MINUTES,new LinkedBlockingQueue<>(),)
+//        new
+    }
+
+    /**
+     * 线程池方式
+     */
+    private static void threadPoolExecute() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int j = 0; j < 1000; j++) {
+                    count.addAndGet(1);
+                }
+            }
+        };
+        for (int i = 0; i < 30; i++) {
+
+            executor.execute(runnable);
+
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()){
+            Thread.yield();
+        }
+        System.out.println("count-"+count);
+    }
+
+    /**
+     * 线程数组方式
+     */
+    private static void threadArrayExecute() {
         Thread[] threads = new Thread[num];
         for (int i = 0; i < num; i++) {
             threads[i] = new Thread(new Runnable() {
