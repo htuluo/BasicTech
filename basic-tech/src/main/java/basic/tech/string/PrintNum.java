f2d55c033452a0f5e1e037814b845f948d6b95f0
luoleiming <luoleiming@yingu.com>
2019-07-03 15:37:01 +0800

    输入数字n，打印长度n的数，自我实现

--- basic-tech/src/main/java/basic/tech/string/PrintNum.java
+++ basic-tech/src/main/java/basic/tech/string/PrintNum.java
@@ -1,5 +1,6 @@
 package basic.tech.string;
 
+import javax.xml.transform.Source;
 import java.io.IOException;
 
 /**
@@ -12,24 +13,72 @@ import java.io.IOException;
 public class PrintNum {
     public static void main(String[] args) {
         try {
-            int n=2;
+            int n=3;
             if (n<0){
                 System.out.println("n cannot be negative");
                 return;
             }
-            for (int i=0;i<n;i++){
-                StringBuilder sb=new StringBuilder();
-                printOne(sb);
-            }
+            Print1ToMaxOfNDigits_2(n);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static StringBuilder printOne(StringBuilder sb) {
-        for (int j=0;j<10;j++){
-            System.out.println(j);
+    public static void Print1ToMaxOfNDigits_2(int n) {
+
+        if (n <= 0) {
+            return;
         }
-        return sb;
+        StringBuffer number = new StringBuffer();
+
+        for (int i = 0; i < n; i++) {
+            number.append('0');
+
+        }
+
+        while (!Increment(number)) {
+            PrintNumber(number);
+        }
     }
+
+    public static boolean Increment(StringBuffer s) {
+        boolean isOverflow = false;
+        int nTakeOver = 0;
+        int nLength = s.length();
+        for (int i = nLength - 1; i >= 0; i--) {
+            int nSum = s.charAt(i) - '0' + nTakeOver;
+            if (i == nLength - 1) {
+                nSum++;
+            }
+            if (nSum >= 10) {
+                if (i == 0) {
+                    isOverflow = true;
+
+                } else {
+                    nSum -= 10;
+                    nTakeOver = 1;
+                    s.setCharAt(i, (char) ('0' + nSum));
+                }
+
+            } else {
+                s.setCharAt(i, (char) ('0' + nSum));
+                break;
+            }
+        }
+        return isOverflow;
+    }
+    public static void PrintNumber(StringBuffer s){
+        boolean isBeginning0 = true;
+        for(int i = 0; i < s.length(); i++){
+            if(isBeginning0 && s.charAt(i) != '0'){
+                isBeginning0 = false;
+            }
+            if(!isBeginning0){
+                System.out.print(s.charAt(i));
+            }
+        }
+
+        System.out.println();
+    }
+
 }
---
+++ basic-tech/src/main/java/basic/tech/string/PrintNum0.java
@@ -0,0 +1,101 @@
+package basic.tech.string;
+
+/**
+ * @description:
+ * @author: luolm
+ * @createTime： 2019/4/23
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class PrintNum0 {
+    public static void main(String[] args) {
+        try {
+            int n = 5;
+            if (n <=0) {
+                System.out.println("n cannot be negative");
+                return;
+            }
+            Print1ToMaxOfNDigits_2(n);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void Print1ToMaxOfNDigits_2(int n) {
+        String[] array = new String[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = "0";
+        }
+        while (!incrementOne(array)) {
+            printArrayToStr(array);
+        }
+    }
+
+    private static boolean incrementOne(String[] array) {
+        boolean overFlow = false;
+        int n = array.length;
+        for (int i = n - 1; i >= 0; i--) {
+            int addValue = Integer.parseInt(array[i]) + 1;
+            boolean needUpFlag = false;
+
+            if (addValue < 10) {
+                array[i] = String.valueOf(addValue);
+                return overFlow;
+            } else {
+                needUpFlag = true;
+                if (i == 0) {
+                    return true;
+                } else {
+                    array[i] = "0";
+                    for (int j = i - 1; j >= 0; j--) {
+                        if (needUpFlag) {
+                            addValue = Integer.parseInt(array[j]) + 1;
+                        }
+
+                        if (addValue < 10) {
+                            array[j] = String.valueOf(addValue);
+                            return overFlow;
+                        } else {
+                            if (j == 0) {
+                                overFlow = true;
+                                return overFlow;
+                            }
+                            array[j] = "0";
+                            needUpFlag = true;
+                            continue;
+                        }
+                    }
+                }
+            }
+            return overFlow;
+        }
+
+
+        return overFlow;
+    }
+
+
+    private static void printArrayToStr(String[] array) {
+        boolean flag = false;
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean begin0 = true;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals("0")) {
+                if (begin0) {
+                    continue;
+                } else {
+                    stringBuilder.append(array[i]);
+
+                }
+            } else {
+                begin0 = false;
+                stringBuilder.append(array[i]);
+            }
+
+        }
+        if (stringBuilder.toString().length() == 0) {
+            stringBuilder.append("0");
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
