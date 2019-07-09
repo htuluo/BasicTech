efdd3b1c2b870e9e26fe61f034d7101f9a374f10
luoleiming <luoleiming@yingu.com>
2019-07-09 10:49:30 +0800

    clone浅拷贝

--- basic-tech/src/main/java/basic/tech/InitClass.java
+++ basic-tech/src/main/java/basic/tech/InitClass.java
@@ -1,18 +1,38 @@
 package basic.tech;
 
-public class InitClass {
-	static final String Hello = "hello";
+public class InitClass implements Cloneable {
+    static final String Hello = "hello";
+    private String proper;
 
-	public InitClass() {
-		System.out.println("init:" + Hello);
-		// TODO Auto-generated constructor stub
-	}
+    public String getProper() {
+        return proper;
+    }
 
-	public static void main(String[] args) {
-		InitClass initClass = new InitClass();
-		System.out.println(InitClass.Hello);
-		// TODO Auto-generated method stub
+    public void setProper(String proper) {
+        this.proper = proper;
+    }
 
-	}
+    public InitClass() {
+        System.out.println("init:" + Hello);
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        InitClass initClass = new InitClass();
+        initClass.setProper("aaa");
+
+        System.out.println("initClass:" + initClass.toString() + " and proper =" + initClass.getProper());
+        try {
+            InitClass clone = (InitClass) initClass.clone();
+            clone.setProper("bbb");
+            System.out.println(clone.toString() + " and proper =" + clone.getProper());
+            System.out.println("initClass:" + initClass.toString() + " and proper =" + initClass.getProper());
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(InitClass.Hello);
+        // TODO Auto-generated method stub
+
+    }
 
 }
