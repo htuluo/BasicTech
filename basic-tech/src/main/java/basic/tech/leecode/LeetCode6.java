ae18bdbbee1e054c9695908f71dbc748105ca742
luoleiming <luoleiming@yingu.com>
2019-07-15 16:30:15 +0800

    第6题，数字反转

---
+++ basic-tech/src/main/java/basic/tech/leecode/LeetCode6.java
@@ -0,0 +1,45 @@
+package basic.tech.leecode;
+
+/**
+ * @description:
+ * @author: luolm
+ * @createTime： 2019/7/15
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class LeetCode6 {
+    public static void main(String[] args) {
+//        Integer integer = new Integer("-111111");
+//        System.out.println(integer);
+        int x=(1<<31)-1;
+        String s=String.valueOf(x);
+        String reverse=null;
+        if (s.startsWith("-")){
+            reverse= reverse(s.substring(1).toCharArray());
+            reverse="-"+reverse;
+        }else {
+            reverse = reverse(s.toCharArray());
+        }
+        try {
+            System.out.println(new Integer(reverse));
+
+        }catch (Exception e){
+            System.out.println(0);
+        }
+    }
+    public static String reverse(char[] chars){
+        StringBuilder stringBuilder=new StringBuilder("");
+        boolean start=false;
+        for (int i=chars.length-1;i>=0;i--){
+            if (chars[i]=='0'&&start){
+                start=true;
+                continue;
+            }else {
+                start=false;
+                stringBuilder.append(chars[i]);
+            }
+
+        }
+        return stringBuilder.toString();
+    }
+}
