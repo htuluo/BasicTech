de7bc66c1189e8a427d8fd1d2c759670dd8f5c40
luoleiming <luoleiming@yingu.com>
2019-10-14 16:24:59 +0800

    LeetCode 673题，最长子序列的个数，第一步，最长子序列的长度递归实现

--- basic-tech/src/main/java/basic/tech/leetcode/LeetCode673.java
+++ basic-tech/src/main/java/basic/tech/leetcode/LeetCode673.java
@@ -1,5 +1,8 @@
 package basic.tech.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @description: 最长递增子序列的个数
  * @author: luolm
@@ -10,9 +13,18 @@ package basic.tech.leetcode;
 public class LeetCode673 {
     public static void main(String[] args) {
         int[] array = {0, 1, 2, 3, 0, 5, 4, 8, 7, 9};
-        System.out.println(findLongerIncreSubArray(array, 0));
+//        System.out.println(findLongerIncreSubArray(array, 0));
+        ArrayList<Integer> list = new ArrayList<>();
+        findLongerIncreSubArray(array,0, list);
+        System.out.println(list.size());
     }
 
+    /**
+     * 递归实现子序列的最大长度
+     * @param array
+     * @param start
+     * @return
+     */
     public static int findLongerIncreSubArray(int[] array, int start) {
         int maxLength = 0, maxCount = 0;
         if (start == array.length - 1) {
@@ -24,6 +36,18 @@ public class LeetCode673 {
             return findLongerIncreSubArray(array, start + 1);
         }
 
+    }
+
+    public static int findLongerIncreSubArray(int[] array, int start, List<Integer> list) {
+        if (start == array.length - 1) {
+            return 1;
+        }
+        if (array[start] < array[start + 1]) {
+            list.add(array[start]);
+            return 1 + findLongerIncreSubArray(array, start + 1,list);
+        } else {
+            return findLongerIncreSubArray(array, start + 1,list);
+        }
 
     }
 }
