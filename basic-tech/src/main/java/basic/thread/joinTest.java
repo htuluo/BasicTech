b2beb7865afddf991b5a198d7892f77ccb769251
luoleiming <luoleiming@yingu.com>
2019-10-17 10:42:52 +0800

    thread join

--- basic-tech/src/main/java/basic/thread/joinTest.java
+++ basic-tech/src/main/java/basic/thread/joinTest.java
@@ -5,11 +5,15 @@ package basic.thread;
  */
 public class joinTest {
     static volatile Integer count = 0;
+    static {
+        System.out.println("Class init");
+    }
 
     public static void main(String[] args) throws InterruptedException {
         Thread threada = new Thread(new Runnable() {
             @Override
             public void run() {
+
                 System.out.println("1");
             }
         });
@@ -17,25 +21,35 @@ public class joinTest {
         Thread threadb = new Thread(new Runnable() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("2");
             }
         });
         threada.start();
+        threada.join();
+        System.out.println("main a join finished");
         threadb.start();
-        while (true) {
-            if (count % 2 == 0) {
-                if (threada.isAlive()) {
-                    threada.wait();
-                }
-                threadb.notify();
-            } else {
-                threada.notify();
-                if (threadb.isAlive()) {
-
-                    threadb.wait();
-                }
-            }
-        }
+        System.out.println("main start");
+        threadb.join();
+        System.out.println("main finished");
+//        while (true) {
+//            if (count % 2 == 0) {
+//                if (threada.isAlive()) {
+//                    threada.wait();
+//                }
+//                threadb.notify();
+//            } else {
+//                threada.notify();
+//                if (threadb.isAlive()) {
+//
+//                    threadb.wait();
+//                }
+//            }
+//        }
 
 
     }
