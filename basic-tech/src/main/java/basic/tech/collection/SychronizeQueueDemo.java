dff6503e1e11efdedefeb240a9a7064e76d77a07
luoleiming <luoleiming@yingu.com>
2019-10-28 15:55:03 +0800

    SynchronousQueue的使用，

---
+++ basic-tech/src/main/java/basic/tech/collection/SychronizeQueueDemo.java
@@ -0,0 +1,50 @@
+package basic.tech.collection;
+
+import java.util.Random;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * @description: SynchronousQueue的使用，
+ * @author: luolm
+ * @createTime： 2019/10/28
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class SychronizeQueueDemo {
+    public static void main(String[] args) {
+        SynchronousQueue<String> queue = new SynchronousQueue<>();
+        new Thread(() -> {
+            try {
+                queue.put("a");
+
+                queue.put("b");
+                queue.put("c");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "AA").start();
+        new Thread(() -> {
+            try {
+                System.out.println(queue.take());
+
+                int bound = 5000;
+                try {
+                    Thread.sleep(new Random().nextInt(bound));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(queue.take());
+                try {
+                    Thread.sleep(new Random().nextInt(bound));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(queue.take());
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "BB").start();
+    }
+}
---
+++ boot-sercurity/src/main/java/java/util/BlockQueue.java
@@ -0,0 +1,11 @@
+package java.util;
+
+/**
+ * @description:
+ * @author: luolm
+ * @createTime： 2019/10/28
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class BlockQueue {
+}
