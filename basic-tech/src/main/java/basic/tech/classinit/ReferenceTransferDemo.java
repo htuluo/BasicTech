1e0700711b78b00e7fc311b7e65a529bff5bfda8
luoleiming <luoleiming@yingu.com>
2019-10-29 17:50:44 +0800

    值传递，引用传递

---
+++ basic-tech/src/main/java/basic/tech/classinit/ReferenceTransferDemo.java
@@ -0,0 +1,28 @@
+package basic.tech.classinit;
+
+/**
+ * @description:
+ * @author: luolm
+ * @createTime： 2019/10/29
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class ReferenceTransferDemo {
+    private String str = "good";
+    private char[] chars = {'a', 'b', 'c'};
+    private int anInt = 3;
+
+    public void change(String a, char[] chars, int anInt) {
+        str = "change";
+        chars[0] = 'k';
+        this.anInt = 9;
+    }
+
+    public static void main(String[] args) {
+        ReferenceTransferDemo referenceTransferDemo = new ReferenceTransferDemo();
+        System.out.println(referenceTransferDemo.str + " " + String.valueOf(referenceTransferDemo.chars) + " " + referenceTransferDemo.anInt);
+        referenceTransferDemo.change(referenceTransferDemo.str, referenceTransferDemo.chars, referenceTransferDemo.anInt);
+        System.out.println(referenceTransferDemo.str + " " + String.valueOf(referenceTransferDemo.chars + " " + referenceTransferDemo.anInt));
+
+    }
+}
