9720d7fd37172ba03f106c9cb870330d8918e697
luoleiming <luoleiming@yingu.com>
2019-10-31 10:44:29 +0800

    weakHashMapTest的使用

--- basic-tech/src/main/java/basic/tech/jvm/RefCountGC.java
+++ basic-tech/src/main/java/basic/tech/jvm/RefCountGC.java
@@ -1,5 +1,7 @@
 package basic.tech.jvm;
 
+import java.util.WeakHashMap;
+
 /**
  * @description: 循环引用的GC演示
  * @author: luolm
@@ -12,6 +14,40 @@ public class RefCountGC {
     private Object instance = null;
 
     public static void main(String[] args) {
+        weakHashMapTest();
+
+    }
+
+    /**
+     * weakHashMap使用，当内存够时不清除，不够时清除所有key
+     *-Xms10m -Xmx10m -XX:+PrintGCDetails
+     */
+    private static void weakHashMapTest() {
+        WeakHashMap<String, Object> map = null;
+        try {
+            map = new WeakHashMap<>();
+            String key = "key";
+            String value = "value";
+//            byte[] b = new byte[10 * 1024 * 1024];
+            byte[] b = new byte[10 * 1024 * 1024];
+            map.put("dd", b);
+            map.put("aa", "ccc");
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "\t=====" + map.get("aa") + "\t" + map.get(key));
+            key = null;
+            System.gc();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "\t=====" + map.get("aa"));
+        }
+    }
+
+
+    /**
+     * 循环引用的示例
+     */
+    private static void cycleReference() {
         //-XX:+PrintGCDetails配置打印详细GC日志
         RefCountGC refA = new RefCountGC();
         RefCountGC refB = new RefCountGC();
