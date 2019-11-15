b72acf189c5fae6e775c94aa9265468d8bfc7b82
luo_leiming <273399098@qq.com>
2019-11-15 16:28:50 +0800

    演示线程报错的demo

---
+++ basic-tech/src/main/java/basic/tech/thread/ThreadExceptionDemo.java
@@ -0,0 +1,40 @@
+package basic.tech.thread;
+
+/**
+ * @description: 演示线程报错的demo
+ * @author: luolm
+ * @createTime： 2019/11/15
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class ThreadExceptionDemo {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new MyRunable());
+        thread.setUncaughtExceptionHandler(new MyUncaultExceptionHandler());
+        try {
+            //加不加try效果一样
+            thread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Catch ========");
+        }
+        System.out.println("===========");
+    }
+
+}
+
+class MyRunable implements Runnable {
+    @Override
+    public void run() {
+        throw new RuntimeException("dddd");
+    }
+}
+
+class MyUncaultExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("uncaughtException catch");
+    }
+}
