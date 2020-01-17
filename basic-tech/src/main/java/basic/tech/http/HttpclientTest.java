ec8215488ff03bb867f028f8e020d22dade6009f
luo_leiming <273399098@qq.com>
2020-01-17 23:09:23 +0800

    不显式关闭

--- basic-tech/src/main/java/basic/tech/http/HttpclientTest.java
+++ basic-tech/src/main/java/basic/tech/http/HttpclientTest.java
@@ -6,6 +6,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -13,6 +14,7 @@ import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author luolm
@@ -21,6 +23,26 @@ import java.nio.charset.Charset;
  */
 public class HttpclientTest {
     public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+
+                    post2();
+                }
+
+            }
+        }).start();
+
+        try {
+            TimeUnit.SECONDS.sleep(12);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void post() {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
             HttpGet httpget = new HttpGet("http://www.baidu.com/");
@@ -46,16 +68,52 @@ public class HttpclientTest {
             String responseBody = httpclient.execute(httpget, responseHandler);
             System.out.println("----------------------------------------");
             System.out.println(responseBody);
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try {
-                httpclient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+//            try {
+//                httpclient.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    public static void post2() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("http://www.baidu.com/");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            CloseableHttpResponse httpResponse = httpclient.execute(httpget);
+
+            // Create a custom response handler
+            String responseBody = EntityUtils.toString(httpResponse.getEntity());
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+//            try {
+//                httpclient.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 }
