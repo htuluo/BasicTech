bdbc8cbb2a57300608963998fc0ad301fe82cdb0
luo_leiming <273399098@qq.com>
2020-01-18 11:51:35 +0800

    httpclient stream 不关闭

--- basic-tech/src/main/java/basic/tech/http/HttpclientTest.java
+++ basic-tech/src/main/java/basic/tech/http/HttpclientTest.java
@@ -4,15 +4,18 @@ package basic.tech.http;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.CharArrayBuffer;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
@@ -26,16 +29,16 @@ public class HttpclientTest {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 500; i++) {
 
-                    post2();
+                    postWithStream();
                 }
 
             }
         }).start();
 
         try {
-            TimeUnit.SECONDS.sleep(12);
+            TimeUnit.SECONDS.sleep(60);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -86,7 +89,10 @@ public class HttpclientTest {
         }
     }
 
-    public static void post2() {
+    /**
+     * util消費
+     */
+    public static void postWithEntityUtils() {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
             HttpGet httpget = new HttpGet("http://www.baidu.com/");
@@ -99,6 +105,53 @@ public class HttpclientTest {
             String responseBody = EntityUtils.toString(httpResponse.getEntity());
             System.out.println("----------------------------------------");
             System.out.println(responseBody);
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+//            try {
+//                httpclient.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    /**
+     *
+     */
+    public static void postWithStream() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("http://www.baidu.com/");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            CloseableHttpResponse httpResponse = httpclient.execute(httpget);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            InputStream inputStream = httpEntity.getContent();
+            Reader reader= new InputStreamReader(inputStream, Charset.forName("utf-8"));
+            char[] tmp = new char[1024];
+            int l;
+            int i = (int)httpEntity.getContentLength();
+            if (i < 0) {
+                i = 4096;
+            }
+            CharArrayBuffer buffer = new CharArrayBuffer(i);
+            while((l = reader.read(tmp)) != -1) {
+                buffer.append(tmp, 0, l);
+            }
+
+            String var9 = buffer.toString();
+            // Create a custom response handler
+            System.out.println("----------------------------------------");
+            System.out.println(var9);
 //            try {
 //                TimeUnit.SECONDS.sleep(1);
 //            } catch (InterruptedException e) {
