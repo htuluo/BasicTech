772d14a7e442f538699e99e81045513b28972084
yodo11 <luoleiming@yodo1.com>
2020-02-06 21:40:21 +0800

    HttpclientTest.java  不关闭连接出现time_wait

--- basic-tech/src/main/java/basic/tech/http/HttpclientTest.java
+++ basic-tech/src/main/java/basic/tech/http/HttpclientTest.java
@@ -26,19 +26,24 @@ import java.util.concurrent.TimeUnit;
  */
 public class HttpclientTest {
     public static void main(String[] args) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 500; i++) {
 
-                    postWithStream();
+        for (int i = 0; i < 20; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        postWithEntityUtils();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+            }).start();
 
-            }
-        }).start();
+        }
+
 
         try {
-            TimeUnit.SECONDS.sleep(60);
+            TimeUnit.SECONDS.sleep(30);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -92,29 +97,33 @@ public class HttpclientTest {
     /**
      * util消費
      */
-    public static void postWithEntityUtils() {
+    public static void postWithEntityUtils() throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse httpResponse = null;
         try {
             HttpGet httpget = new HttpGet("http://www.baidu.com/");
+            httpResponse = httpclient.execute(httpget);
 
             System.out.println("Executing request " + httpget.getRequestLine());
 
-            CloseableHttpResponse httpResponse = httpclient.execute(httpget);
 
             // Create a custom response handler
-            String responseBody = EntityUtils.toString(httpResponse.getEntity());
-            System.out.println("----------------------------------------");
-            System.out.println(responseBody);
-//            try {
-//                TimeUnit.SECONDS.sleep(1);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("--------before EntityUtils--------------------------------");
+
+//            String responseBody = EntityUtils.toString(httpResponse.getEntity());
+//            System.out.println("----------after EntityUtils------------------------------");
+//            System.out.println(responseBody);
         } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+//            httpResponse.close();
 //            try {
 //                httpclient.close();
 //            } catch (IOException e) {
@@ -136,15 +145,15 @@ public class HttpclientTest {
             CloseableHttpResponse httpResponse = httpclient.execute(httpget);
             HttpEntity httpEntity = httpResponse.getEntity();
             InputStream inputStream = httpEntity.getContent();
-            Reader reader= new InputStreamReader(inputStream, Charset.forName("utf-8"));
+            Reader reader = new InputStreamReader(inputStream, Charset.forName("utf-8"));
             char[] tmp = new char[1024];
             int l;
-            int i = (int)httpEntity.getContentLength();
+            int i = (int) httpEntity.getContentLength();
             if (i < 0) {
                 i = 4096;
             }
             CharArrayBuffer buffer = new CharArrayBuffer(i);
-            while((l = reader.read(tmp)) != -1) {
+            while ((l = reader.read(tmp)) != -1) {
                 buffer.append(tmp, 0, l);
             }
 
