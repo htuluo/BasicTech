6f20a413dad3649df1735f7261502d9d471d9295
luo_leiming <273399098@qq.com>
2020-06-22 22:18:37 +0800

    查找某个数的素数

--- basic-tech/src/main/java/basic/tech/prime/ConcurrentPrimeClass.java
+++ basic-tech/src/main/java/basic/tech/prime/ConcurrentPrimeClass.java
@@ -1,4 +1,4 @@
-package basic.thread;
+package basic.tech.prime;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -67,6 +67,7 @@ public class ConcurrentPrimeClass {
         }
         //先从缓存list中查找
         if (!LIST_CACHE.isEmpty() && LIST_CACHE.size() != 0 && num < MAX_NUM) {
+            System.out.println("从缓存拿：" + num);
             list = (ArrayList) LIST_CACHE.parallelStream().filter(item ->
                     Integer.compare((int) item, num) <= 0
             ).collect(Collectors.toList());
@@ -99,6 +100,33 @@ public class ConcurrentPrimeClass {
         }
 
         return LIST_CACHE;
+    }
+
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        ConcurrentPrimeClass concurrentPrimeClass = new ConcurrentPrimeClass();
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                countDownLatch.countDown();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(new Random().nextInt(500));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                int num = new Random().nextInt(6000);
+                List<Integer> primeForNum = concurrentPrimeClass.findPrimeForNum(num);
+                System.out.println(num + "的数字：" + primeForNum);
+
+            }).start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=========");
+
     }
 
 }
---
+++ basic-tech/src/main/java/basic/tech/prime/PrimeClass.java
@@ -0,0 +1,59 @@
+package basic.tech.prime;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @description:
+ * @author: luolm
+ * @createTime： 2020/6/22
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class PrimeClass {
+    /**
+     * 查找某个正整数的所有质数
+     *
+     * @param num
+     * @return
+     */
+    public List<Integer> findPrimeForNum(int num) {
+        List<Integer> list = new LinkedList<>();
+        if (num < 2) {
+            return list;
+        }
+        for (int i = 2; i <= num; i++) {
+            if (isPrime(i)) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 判断某整数是否为质数
+     *
+     * @param num
+     * @return
+     */
+    public boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        int count = (int) Math.sqrt(num) + 1;
+        for (int i = 2; i < count; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        PrimeClass primeClass = new PrimeClass();
+        System.out.println(primeClass.findPrimeForNum(200));
+    }
+
+
+}
