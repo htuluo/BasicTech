a2738ef93c1dc081250378dd0955f0eb85d9ca94
luo_leiming <273399098@qq.com>
2020-06-22 12:09:26 +0800

    打印n位数和

--- basic-tech/src/main/java/basic/tech/leetcode/PrintNumForSum.java
+++ basic-tech/src/main/java/basic/tech/leetcode/PrintNumForSum.java
@@ -1,5 +1,8 @@
 package basic.tech.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @description: 打印n位数 和
  * @author: luolm
@@ -9,28 +12,49 @@ package basic.tech.leetcode;
  */
 public class PrintNumForSum {
     public static void main(String[] args) {
-        PrintNumForSum printNumForSum = new PrintNumForSum();
-        printNumForSum.printNume(10, 3);
+//        PrintNumForSum printNumForSum = new PrintNumForSum();
+//        printNumForSum.printNume(10, 3);
+        printNume(9, 3);
+
 
     }
 
-    private void printNume(int sum, int count) {
+    private static void printNume(int sum, int count) {
         StringBuilder sb = new StringBuilder();
 
-        while (count > 0) {
-            sb = oneLevel(sb, sum, count--);
-            System.out.println(sb.toString());
-        }
+        ArrayList<String> arrayList = new ArrayList<>();
+        oneLevel(arrayList, sb, sum, count);
+        System.out.println(arrayList);
 
     }
 
-    private StringBuilder oneLevel(StringBuilder stringBuilder, int sum, int count) {
-        int sumByBuilder = getSumByBuilder(stringBuilder);
-        for (int i = 0; i < sum - sumByBuilder; i++) {
-            stringBuilder.append(String.valueOf(i));
-            oneLevel(stringBuilder, sum - sumByBuilder, count);
+    private static void oneLevel(List<String> list, StringBuilder stringBuilder, int sum, int count) {
+        while (count > 2) {
+
+            for (int i = 0; i < 8; i++) {
+                int j = sum - i;
+                if (j >= 0) {
+                    stringBuilder.append(String.valueOf(i));
+
+                    oneLevel(list, stringBuilder, j, --count);
+                    stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+                } else {
+                    continue;
+                }
+            }
         }
-        return stringBuilder;
+        if (count == 2) {
+            for (int i = 0; i < 8; i++) {
+                int j = sum - i;
+                if (j < 0 || j > 7) {
+                    continue;
+                }
+                stringBuilder.append(String.valueOf(i)).append(String.valueOf(j));
+                list.add(stringBuilder.toString());
+                stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+//
+            }
+        }
 
     }
 
@@ -41,4 +65,26 @@ public class PrintNumForSum {
         }
         return result;
     }
+
+
+    public static void printSubForSum(int n) {
+        int[][] sum = new int[n + 1][n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            sum[i][1] = 1;
+            //最开始都为1
+        }
+        for (int i = 1; i < n + 1; i++) {
+            //接下来至少一个为2
+            for (int j = 2; j < i + 1; j++) {
+                sum[i][j] = sum[i - 1][j - 1] + sum[i - j][j];
+            }
+            int s = 0;
+            for (int j = 1; j < n + 1; j++) {
+                s += sum[n][j];
+                System.out.println(s);
+            }
+
+        }
+    }
+
 }
