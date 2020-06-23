b512d2686297e758a61fda8aa8248d2bfdafd159
luo_leiming <273399098@qq.com>
2020-06-23 15:58:53 +0800

    欧拉筛选法实现

---
+++ basic-tech/src/main/java/basic/tech/prime/ConcurrentPrime2Class.java
@@ -0,0 +1,124 @@
+package basic.tech.prime;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
+
+/**
+ * @description: 查找小于某数的所有素数
+ * @author: luolm
+ * @createTime： 2020/6/22
+ * @version: v1.0.0
+ * @history: (版本) 作者 时间 注释
+ */
+public class ConcurrentPrime2Class {
+
+    /**
+     * 缓存已处理的数的素数列表
+     */
+    private static List<Integer> LIST_CACHE= new ArrayList<>();
+    /**
+     * 缓存最大处理过的数
+     */
+    private static Integer MAX_NUM=2;
+
+    private static ReentrantLock lock = new ReentrantLock();
+
+    /**
+     * 查找某个正整数的所有质数
+     *
+     * @param num
+     * @return
+     */
+    public List<Integer> findPrimeForNum(int num) {
+        List<Integer> list = new ArrayList<>();
+        if (num < 2) {
+            return list;
+        }
+        //先从缓存list中查找
+        if (!LIST_CACHE.isEmpty() && LIST_CACHE.size() != 0 && num <= MAX_NUM) {
+            System.out.println("从缓存拿：" + num);
+            list = LIST_CACHE.stream().filter(item ->
+                    Integer.compare((int) item, num) <= 0
+            ).collect(Collectors.toList());
+            return list;
+        }
+
+        lock.lock();
+        try {
+            LIST_CACHE = this.eulerPrimeFindList(num);
+            MAX_NUM = num;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            lock.unlock();
+        }
+
+        return LIST_CACHE;
+    }
+
+    /**
+     * 欧拉筛选
+     *
+     * @param num
+     * @return
+     */
+    public List<Integer> eulerPrimeFindList(int num) {
+        // flagArr[i]存储标志过的合数；prime数组保存所有质数
+        boolean[] flagArr = new boolean[num];
+        List<Integer> primeList = new ArrayList<>();
+        int count = 0;  // 质数数量
+        for (int i = 2; i < num; i++) {
+            if (!flagArr[i]) {
+                primeList.add(count++, i);
+            }
+            // 剔除已筛选的素数的倍数
+            for (int j = 0; j < count; j++) {
+                if (i * primeList.get(j) >= num) {
+                    break;
+                }
+                flagArr[i * primeList.get(j)] = true;
+                //之前标记过不再处理
+                if (i % primeList.get(j) == 0) {
+                    break;
+                }
+            }
+        }
+        return primeList;
+    }
+
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        ConcurrentPrime2Class concurrentPrimeClass = new ConcurrentPrime2Class();
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                countDownLatch.countDown();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(new Random().nextInt(500));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                int num = new Random().nextInt(500000);
+                long start = System.currentTimeMillis();
+                List<Integer> primeForNum = concurrentPrimeClass.findPrimeForNum(num);
+                System.out.println(Thread.currentThread().getName() + " cost:" + (System.currentTimeMillis() - start));
+//                System.out.println(num + "的数字：" + primeForNum);
+
+            }).start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=========");
+
+    }
+
+}
--- basic-tech/src/main/java/basic/tech/prime/PrimeClass.java
+++ basic-tech/src/main/java/basic/tech/prime/PrimeClass.java
@@ -69,14 +69,95 @@ public class PrimeClass {
     }
 
 
+    /**
+     * 埃式筛选
+     *
+     * @param n
+     * @return
+     */
+    public int[] eratosthenesPrimeFind(int n) {
+        boolean[] flagArr = new boolean[n];
+        int[] prime = new int[n];  // flagArr[i]是i的最小质因子；prime数组保存所有质数
+        int count = 0;  // 质数数量
+        for (int i = 2; i < n; i++) {
+            if (!flagArr[i]) {
+                prime[count++] = i;
+            }  // i是质数
+            // 剔除 indexI 的倍数
+            for (int indexJ = i + i; indexJ < n; indexJ += i) {
+                flagArr[indexJ] = true;
+            }
+        }
+        return prime;
+    }
+
+    /**
+     * 欧拉筛选
+     *
+     * @param n
+     * @return
+     */
+    public int[] eulerPrimeFind(int n) {
+        boolean[] flagArr = new boolean[n];
+        int[] prime = new int[n];  // flagArr[i]是i的最小质因子；prime数组保存所有质数
+        int count = 0;  // 质数数量
+        for (int i = 2; i < n; i++) {
+            if (!flagArr[i]) {
+                prime[count++] = i;
+            }  // i是质数
+            // 剔除 i 的倍数
+            for (int j = 0; j < count; j++) {
+                if (i * prime[j] >= n) {
+                    break;
+                }
+                flagArr[i * prime[j]] = true;
+                if (i % prime[j] == 0) {
+                    break;
+                }
+            }
+        }
+        return prime;
+    }
+
+    /**
+     * 欧拉筛选
+     *
+     * @param num
+     * @return
+     */
+    public List<Integer> eulerPrimeFindList(int num) {
+        // flagArr[i]存储标志；primeList保存所有质数
+        boolean[] flagArr = new boolean[num];
+        List<Integer> primeList = new ArrayList<>();
+        int count = 0;  // 质数数量
+        for (int i = 2; i < num; i++) {
+            if (!flagArr[i]) {
+                primeList.add(count++, i);
+            }
+            //剔除 i 的倍数
+            for (int j = 0; j < count; j++) {
+                if (i * primeList.get(j) >= num) {
+                    break;
+                }
+                flagArr[i * primeList.get(j)] = true;
+                if (i % primeList.get(j) == 0) {
+                    break;
+                }
+            }
+        }
+        return primeList;
+    }
+
     public static void main(String[] args) {
         PrimeClass primeClass = new PrimeClass();
         long start = System.currentTimeMillis();
 //        Set<Integer> primeList = primeClass.findPrimeSetForNum(100000000);
-        List<Integer> primeList = primeClass.findPrimeForNum(100000000);
-        System.out.println("cost:" + (System.currentTimeMillis() - start) / 1000);
+//        List<Integer> primeList = primeClass.findPrimeForNum(100000000);
+//        int[] ints = primeClass.eulerPrimeFind(100000000);
+        List<Integer> primeList = primeClass.eulerPrimeFindList(100000000);
+        System.out.println("cost:" + (System.currentTimeMillis() - start));
         start = System.currentTimeMillis();
-        int filter = 4000000;
+        int filter = 50000000;
         System.out.println(primeList.stream().filter(item -> item < filter).collect(Collectors.toList()).size());
         System.out.println("stream cost:" + (System.currentTimeMillis() - start));
         start = System.currentTimeMillis();
