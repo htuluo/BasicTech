7665b29a69734e6edaa7b946a8fb35afcd334cac
luo_leiming <273399098@qq.com>
2020-07-20 15:53:12 +0800

    子类调用父类静态属性时不初始化子类

--- basic-tech/src/main/java/basic/tech/StaticTest.java
+++ basic-tech/src/main/java/basic/tech/StaticTest.java
@@ -5,15 +5,24 @@ public class StaticTest {
 		static Integer A = 1;
 		static {
 			A = 2;
+			System.out.println("Parent static init");
 		}
 	}
 
 	static class Child extends Parent {
 		public static Integer B = A=3;
+		static {
+			System.out.println("child static init");
+		}
+
+		{
+			System.out.println("child non-static init");
+
+		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println(Child.A);
+		System.out.println(Child.B);
 	}
 
 }
