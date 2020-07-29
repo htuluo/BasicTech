be133bb84539dbedc1556e828b14e44eda09d9ae
luo_leiming <273399098@qq.com>
2020-07-29 11:34:49 +0800

    普通类的内存结构

--- basic-tech/src/main/java/basic/tech/lock/SynchronizeLayout.java
+++ basic-tech/src/main/java/basic/tech/lock/SynchronizeLayout.java
@@ -10,7 +10,33 @@ import org.openjdk.jol.info.ClassLayout;
  * @history: (版本) 作者 时间 注释
  */
 public class SynchronizeLayout {
+    class TestClass{
+        private Object object;
+        private int intA;
+    }
     public static void main(String[] args) throws InterruptedException {
+        testForCommonClassLayout();
+    }
+
+    /**
+     * 普通类的内存结构
+     * basic.tech.lock.SynchronizeLayout$TestClass object internals:
+     *  OFFSET  SIZE                                TYPE DESCRIPTION                               VALUE
+     *       0     4                                     (object header)                           01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+     *       4     4                                     (object header)                           00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+     *       8     4                                     (object header)                           43 c1 00 20 (01000011 11000001 00000000 00100000) (536920387)
+     *      12     4                                 int TestClass.intA                            0
+     *      16     4                    java.lang.Object TestClass.object                          null
+     *      20     4   basic.tech.lock.SynchronizeLayout TestClass.this$0                          (object)
+     * Instance size: 24 bytes
+     *
+     */
+    public static void testForCommonClassLayout() {
+        TestClass testClass=(new SynchronizeLayout()).new TestClass();
+        System.out.println(ClassLayout.parseInstance(testClass).toPrintable());
+    }
+
+    public static void testForLayout() {
         //先sleep后，对象会加上匿名偏向锁
 //        Thread.sleep(5000);
         Object o = new Object();
