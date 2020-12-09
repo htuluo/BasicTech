4984f3af16877e7744a8134b42bbac0698114186
luoleiming <luoleiming@dangdang.com>
2020-12-09 19:18:45 +0800

    stopwatch guava test

--- java-basic/src/test/java/com/luo/jdbc/TimeTest.java
+++ java-basic/src/test/java/com/luo/jdbc/TimeTest.java
@@ -1,5 +1,7 @@
 package com.luo.jdbc;
 
+import com.google.common.base.Stopwatch;
+import org.apache.commons.lang3.ThreadUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 
@@ -7,6 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ： luoleiming
@@ -25,5 +28,24 @@ public class TimeTest {
         System.out.println(sdf1.format(date));
         System.out.println(sdf2.format(date));
         System.out.println(sdf3.format(date));
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+
+        Stopwatch stopWatch= Stopwatch.createStarted();
+
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("cost time:"+stopWatch.stop());
+        stopWatch.start();
+        Thread.sleep(2012);
+        stopWatch.stop();
+        System.out.println(stopWatch.elapsed());
+        System.out.println(stopWatch.elapsed(TimeUnit.SECONDS));
+        stopWatch.reset().start();
+        Thread.sleep(5012);
+        stopWatch.stop();
+        System.out.println(stopWatch.elapsed(TimeUnit.MILLISECONDS));
+
     }
 }
