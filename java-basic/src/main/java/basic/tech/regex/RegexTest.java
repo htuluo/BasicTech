78f41f88a3f6859db42d04383e8f8e7aa207259b
luoleiming <luoleiming@dangdang.com>
2021-02-07 21:11:36 +0800

    Regex 替换有问题的

--- java-basic/src/main/java/basic/tech/regex/RegexTest.java
+++ java-basic/src/main/java/basic/tech/regex/RegexTest.java
@@ -1,6 +1,8 @@
 package basic.tech.regex;
 
 import javax.sound.midi.Soundbank;
+import java.util.HashMap;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,14 +13,71 @@ import java.util.regex.Pattern;
  */
 public class RegexTest {
     public static void main(String[] args) {
-        String text="$$1$$&&$$2$$";
-        String regex="\\$\\$(\\d+)\\$\\$";
-        Pattern pattern=Pattern.compile(regex);
+        replaceRegex2();
+
+    }
+
+    private static void regexTest() {
+        String text = "$1$$&&$2$||$1$";
+        String regex = "\\$(\\d+)\\$";
+        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
-        while (matcher.find()){
-            text=text.replace(matcher.group(),matcher.group(1));
+        while (matcher.find()) {
+            text = text.replace(matcher.group(), matcher.group(1));
         }
         System.out.println(text);
+    }
+
+    public static void replaceRegex() {
+        String text = "11&&1&&2&&111";
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("11", "a");
+        stringObjectHashMap.put("111", "b");
+        stringObjectHashMap.put("1", "c");
+        String a = null;
+        while (matcher.find()) {
+                String group = matcher.group();
+            Optional<String> first = stringObjectHashMap.keySet().stream().filter(item -> {
+                return item.equals(group);
+            }).findFirst();
+            if (first.isPresent()) {
+                a = matcher.replaceFirst(stringObjectHashMap.get(first.get()).toString());
+                matcher.reset();
+                matcher=pattern.matcher(a);
+
+            }
+        }
+        System.out.println(a.toString());
+
+    }
+
+    public static void replaceRegex2() {
+        String text = "11&&1&&2&&111";
+        String regex = "\\d+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("11", "a");
+        stringObjectHashMap.put("111", "b");
+        stringObjectHashMap.put("1", "c");
+        String a = text;
+        while (matcher.find()) {
+            String group = matcher.group();
+            Optional<String> first = stringObjectHashMap.keySet().stream().filter(item -> {
+                return item.equals(group);
+            }).findFirst();
+            if (first.isPresent()) {
+                StringBuilder sb=new StringBuilder(a);
+                sb.replace(matcher.start(),matcher.end(),stringObjectHashMap.get(first.get()).toString());
+                a=sb.toString();
+                matcher=pattern.matcher(a);
+
+            }
+        }
+        System.out.println(a.toString());
 
     }
 }
