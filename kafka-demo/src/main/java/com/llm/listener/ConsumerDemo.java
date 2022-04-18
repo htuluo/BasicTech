9ba0c93bac04ae6102e6a8de1b02f8d28baf95cb
luoleiming <luoleiming@dangdang.com>
2022-04-18 17:16:43 +0800

    consumer

--- kafka-demo/src/main/java/com/llm/listener/ConsumerDemo.java
+++ kafka-demo/src/main/java/com/llm/listener/ConsumerDemo.java
@@ -2,6 +2,7 @@ package com.llm.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +17,14 @@ import java.text.MessageFormat;
 @Slf4j
 @Component
 public class ConsumerDemo {
+    @Value("${kafka.consumer.sleepSeconds}")
+    private Integer sleepSeconds;
+
     @KafkaListener(topics = "${kafka.consumer.topic}")
     public void onMessage(ConsumerRecord<String, String> consumerRecord) throws InterruptedException {
-        log.info(MessageFormat.format("{0}\t{1}\t{2}", consumerRecord.value(), consumerRecord.offset(), consumerRecord.partition()));
+        log.info(MessageFormat.format("offset:{1}\tpartition:{2}\t{0}\t", consumerRecord.value(), consumerRecord.offset(), consumerRecord.partition()));
         try {
-            Thread.sleep(100);
+            Thread.sleep(sleepSeconds);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
