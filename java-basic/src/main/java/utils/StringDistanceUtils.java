1c91231be0d2df7e25d1dbfe9e3b3c4b110538a0
luoleiming <luoleiming@dangdang.com>
2022-06-24 15:31:25 +0800

    文本距离计算

---
+++ java-basic/src/main/java/utils/StringDistanceUtils.java
@@ -0,0 +1,170 @@
+package utils;
+
+import org.apache.commons.collections4.SetUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/*
+ *@author: luoleiming
+ *@date: 2022/6/23 16:40
+ *@description: 字符串相似度计算：参考https://juejin.cn/post/6844903992812634119
+ */
+public class StringDistanceUtils {
+    public static float jaccard(String a, String b) {
+        if (a == null && b == null) {
+            return 1f;
+        }
+        // 都为空相似度为 1
+        if (a == null || b == null) {
+            return 0f;
+        }
+        Set<Integer> aChar = a.chars().boxed().collect(Collectors.toSet());
+        Set<Integer> bChar = b.chars().boxed().collect(Collectors.toSet());
+        // 交集数量
+        int intersection = SetUtils.intersection(aChar, bChar).size();
+        if (intersection == 0) return 0;
+        // 并集数量
+        int union = SetUtils.union(aChar, bChar).size();
+        return ((float) intersection) / (float) union;
+    }
+
+    public static float SorensenDice(String a, String b) {
+        if (a == null && b == null) {
+            return 1f;
+        }
+        if (a == null || b == null) {
+            return 0F;
+        }
+        Set<Integer> aChars = a.chars().boxed().collect(Collectors.toSet());
+        Set<Integer> bChars = b.chars().boxed().collect(Collectors.toSet());
+        // 求交集数量
+        int intersect = SetUtils.intersection(aChars, bChars).size();
+        if (intersect == 0) {
+            return 0F;
+        }
+        // 全集，两个集合直接加起来
+        int aSize = aChars.size();
+        int bSize = bChars.size();
+        return (2 * (float) intersect) / ((float) (aSize + bSize));
+    }
+
+    public static float Levenshtein(String a, String b) {
+        if (a == null && b == null) {
+            return 1f;
+        }
+        if (a == null || b == null) {
+            return 0F;
+        }
+        int editDistance = editDis(a, b);
+        return 1 - ((float) editDistance / Math.max(a.length(), b.length()));
+    }
+
+    private static int editDis(String a, String b) {
+
+        int aLen = a.length();
+        int bLen = b.length();
+
+        if (aLen == 0) return aLen;
+        if (bLen == 0) return bLen;
+
+        int[][] v = new int[aLen + 1][bLen + 1];
+        for (int i = 0; i <= aLen; ++i) {
+            for (int j = 0; j <= bLen; ++j) {
+                if (i == 0) {
+                    v[i][j] = j;
+                } else if (j == 0) {
+                    v[i][j] = i;
+                } else if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    v[i][j] = v[i - 1][j - 1];
+                } else {
+                    v[i][j] = 1 + Math.min(v[i - 1][j - 1], Math.min(v[i][j - 1], v[i - 1][j]));
+                }
+            }
+        }
+        return v[aLen][bLen];
+    }
+
+    /**
+     * @description:
+     * @param:
+     * @return:
+     * @date: 2022/6/23
+     */
+    public static float hamming(String a, String b) {
+        if (a == null || b == null) {
+            return 0f;
+        }
+        if (a.length() != b.length()) {
+            return 0f;
+        }
+
+        int disCount = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                disCount++;
+            }
+        }
+        return (float) disCount / (float) a.length();
+    }
+
+    /**
+     * @param a
+     * @param b
+     * @return
+     */
+    public static float cos(String a, String b) {
+        if (a == null || b == null) {
+            return 0F;
+        }
+        Set<Integer> aChar = a.chars().boxed().collect(Collectors.toSet());
+        Set<Integer> bChar = b.chars().boxed().collect(Collectors.toSet());
+
+        // 统计字频
+        Map<Integer, Integer> aMap = new HashMap<>();
+        Map<Integer, Integer> bMap = new HashMap<>();
+        for (Integer a1 : aChar) {
+            aMap.put(a1, aMap.getOrDefault(a1, 0) + 1);
+        }
+        for (Integer b1 : bChar) {
+            bMap.put(b1, bMap.getOrDefault(b1, 0) + 1);
+        }
+
+        // 向量化
+        Set<Integer> union = SetUtils.union(aChar, bChar);
+        int[] aVec = new int[union.size()];
+        int[] bVec = new int[union.size()];
+        List<Integer> collect = new ArrayList<>(union);
+        for (int i = 0; i < collect.size(); i++) {
+            aVec[i] = aMap.getOrDefault(collect.get(i), 0);
+            bVec[i] = bMap.getOrDefault(collect.get(i), 0);
+        }
+
+        // 分别计算三个参数
+        int p1 = 0;
+        for (int i = 0; i < aVec.length; i++) {
+            p1 += (aVec[i] * bVec[i]);
+        }
+
+        float p2 = 0f;
+        for (int i : aVec) {
+            p2 += (i * i);
+        }
+        p2 = (float) Math.sqrt(p2);
+
+        float p3 = 0f;
+        for (int i : bVec) {
+            p3 += (i * i);
+        }
+        p3 = (float) Math.sqrt(p3);
+
+        return ((float) p1) / (p2 * p3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringDistanceUtils.jaccard("中化人民解放军", "中国人民解放战争"));
+        System.out.println(StringDistanceUtils.SorensenDice("中化人民解放军", "中国人民解放战争"));
+        System.out.println(StringDistanceUtils.editDis("中化人民解放军", "中国人民解放战争"));
+        System.out.println(StringDistanceUtils.hamming("中化人民解放军", "中国人民解放战争"));
+    }
+}
