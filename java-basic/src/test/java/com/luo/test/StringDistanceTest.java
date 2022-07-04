7442b4d4fe6d88c5380e84d90a5ed7f1cd1b14ad
luoleiming <luoleiming@dangdang.com>
2022-07-04 15:15:43 +0800

    余弦相似度计算benchmark

--- java-basic/src/test/java/com/luo/test/StringDistanceTest.java
+++ java-basic/src/test/java/com/luo/test/StringDistanceTest.java
@@ -33,7 +33,7 @@ public class StringDistanceTest {
 
     @Setup
     public void initList() {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 10000; i++) {
             list.add(aStr + i);
         }
 
