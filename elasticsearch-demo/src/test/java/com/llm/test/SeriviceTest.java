9ce80ec28a03572ca104c679e4c57651cc1a48f9
luo_leiming <273399098@qq.com>
2023-07-14 14:39:15 +0800

    fastJson parseObject

--- elasticsearch-demo/src/test/java/com/llm/test/SeriviceTest.java
+++ elasticsearch-demo/src/test/java/com/llm/test/SeriviceTest.java
@@ -1,5 +1,8 @@
 package com.llm.test;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.llm.domain.RiskOrderDetail;
 import com.llm.service.RiskOrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,5 +23,23 @@ public class SeriviceTest {
     @Test
     public void testUpdate() {
         service.update();
+    }
+    @Test
+    public void testJsonParse() {
+        RiskOrderDetail orderDetail = new RiskOrderDetail();
+        orderDetail.setId("222000");
+        orderDetail.setOrderId(222000L);
+        orderDetail.setOrderIp("127.0.0.1");
+        orderDetail.setOrderIsFlag(56);
+        orderDetail.setOrderStatus(900);
+        orderDetail.setOrderStatus(100);
+        String jsonStr = JSONObject.toJSONString(orderDetail);
+        System.out.println(jsonStr);
+        String json2="{\"id\":\"222000\",\"orderId\":222000,\"orderIp\":\"127.0.0.1\",\"orderIsFlag\":56,\"orderStatus\":100}";
+        ParserConfig config=new ParserConfig(false);
+        config.setAsmEnable(false);
+        config.setJacksonCompatible(true);
+        RiskOrderDetail riskOrderDetail = JSONObject.parseObject(json2, RiskOrderDetail.class);
+        System.out.println(riskOrderDetail);
     }
 }
